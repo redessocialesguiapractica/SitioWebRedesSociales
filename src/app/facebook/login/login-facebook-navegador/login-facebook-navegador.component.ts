ab647a312432a4e9764d4cf8d2d46c6f078c240a
leonardoAL124 <leonardoAL124@users.noreply.github.com>
2024-01-06 18:23:34 -0500

    Entrar - Inicio

--- src/app/facebook/login/login-facebook-navegador/login-facebook-navegador.component.ts
+++ src/app/facebook/login/login-facebook-navegador/login-facebook-navegador.component.ts
@@ -1,10 +1,133 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-login-facebook-navegador',
   templateUrl: './login-facebook-navegador.component.html',
   styleUrls: ['./login-facebook-navegador.component.css']
 })
-export class LoginFacebookNavegadorComponent {
+export class LoginFacebookNavegadorComponent implements OnInit {
+  constructor(private cdRef: ChangeDetectorRef) {}
+  private nextInstructionTimer: any;
+  private audioSubscription: Subscription | undefined;
+  ngOnInit(): void {
+    this.repeatInstruction();
+    this.setupNextInstructionTrigger();
+  }
+  
+  audioInstruccionesData = [
+    {
+      index: 1,
+      content: 'Hola esta es la primera ventana del login!',
+      audio: '/assets/audio/facebook/registro/paso_1.mp3',
+    },
+    {
+      index: 2,
+      content: 'En nuestra pantalla principal del teléfono, presionamos el botón que está siendo destacado, el cual es el botón de la Play Store o tienda de aplicaciones. Presiona el botón para continuar con el siguiente paso.',
+      audio: '/assets/audio/facebook/registro/paso_2.mp3',
+    },
+    {
+      index: 3,
+      content: 'Una vez abierta la Play Store o la tienda de aplicaciones, tendremos una pantalla similar a esta. En esta pantalla, presionaremos sobre la barra de búsqueda para continuar con el siguiente paso.',
+      audio: '/assets/audio/facebook/registro/paso_3.mp3',
+    },
+    {
+      index: 4,
+      content: 'Esta es la pantalla de búsqueda de la tienda de aplicaciones. En la parte superior del teléfono, tendremos una barra de búsqueda con el texto "Buscar apps y juegos". Por favor, escribe "Facebook" dentro del rectángulo rojo desde tu teclado para continuar con el siguiente paso.',
+      audio: '/assets/audio/facebook/registro/paso_4.mp3',
+    },
+    // Add more objects with different content as needed
+  ];
+  currentIndex = 0;
 
+  onAudioInstruccionesButtonClick(buttonText: string): void {
+    switch (buttonText) {
+      case 'Anterior':
+        this.previousInstruction();
+        break;
+      case 'Siguiente':
+        this.nextInstruction();
+        break;
+      case 'Repetir':
+        this.repeatInstruction();
+        break;
+      // Add more cases for other buttons or triggers as needed
+    }
+  }
+
+  nextInstruction(): void {
+    if (this.currentIndex < this.audioInstruccionesData.length - 1) {
+      this.currentIndex++;
+      this.clearNextInstructionTrigger(); // clear existing timer
+      this.setupNextInstructionTrigger();
+    }
+  }
+  
+  previousInstruction(): void {
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+      this.clearNextInstructionTrigger(); // clear existing timer
+      this.setupNextInstructionTrigger();
+    }
+  }
+
+  repeatInstruction(): void {
+    // Handle repeat logic if needed
+  }
+
+  setupNextInstructionTrigger(): void {
+    // Clear any existing triggers
+    this.clearNextInstructionTrigger();
+  
+    // Set up trigger based on current index
+    switch (this.currentIndex) {
+      case 0:
+        // Set up a timer for the next step after 30 seconds
+        this.nextInstructionTimer = setTimeout(() => {
+          this.nextInstruction();
+        }, 30000);
+        break;
+      // Add more cases for other steps
+    }
+  }
+
+  ngOnDestroy() {
+    // Unsubscribe to prevent memory leaks
+    if (this.audioSubscription) {
+      this.audioSubscription.unsubscribe();
+    }
+
+    // Clear any existing triggers
+    this.clearNextInstructionTrigger();
+  }
+
+  clearNextInstructionTrigger(): void {
+    // Clear any existing triggers, such as timers or event listeners
+    // This will depend on the specific requirements for clearing triggers
+  
+    // Clear the timer if it's set
+    if (this.nextInstructionTimer) {
+      clearTimeout(this.nextInstructionTimer);
+      this.nextInstructionTimer = null; // Reset the timer variable
+    }
+  }
+
+
+  //Method for advancing to the next step
+  facebookRegistroClicked(): void {
+    // Call the nextInstruction method or perform other actions specific to index 1
+    this.nextInstruction();
+  }
+
+   //Method especific for index 3
+   facebookRegistroP4InputChanged(event: Event): void {
+    const target = event.target as HTMLInputElement;
+    const searchText = target.value.toLowerCase(); // Convert to lowercase for case-insensitive comparison
+    if (searchText.includes('facebook')) {
+      this.nextInstructionTimer = setTimeout(() => {
+        this.nextInstruction();
+      }, 2000);
+      
+    }
+  }
 }

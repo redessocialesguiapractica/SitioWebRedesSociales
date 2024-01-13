25b7de833ee77c3ddf6c40722e77dea74fb2bece
lizlopez24 <133879942+lizlopez24@users.noreply.github.com>
2024-01-12 22:24:11 -0500

    Primeros 3 pasos

--- src/app/facebook/enviarMensajes/enviar-mensajes-navegador/enviar-mensajes-navegador.component.ts
+++ src/app/facebook/enviarMensajes/enviar-mensajes-navegador/enviar-mensajes-navegador.component.ts
@@ -1,10 +1,116 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-enviar-mensajes-navegador',
   templateUrl: './enviar-mensajes-navegador.component.html',
   styleUrls: ['./enviar-mensajes-navegador.component.css']
 })
-export class EnviarMensajesNavegadorComponent {
+export class EnviarMensajesNavegadorComponent implements OnInit {
+  constructor(private cdRef: ChangeDetectorRef) {}
+  private nextInstructionTimer: any;
+  private audioSubscription: Subscription | undefined;
+  ngOnInit(): void {
+    this.repeatInstruction();
+    this.setupNextInstructionTrigger();
+  }
+  audioInstruccionesData = [
+    {
+      index: 1,
+      content:
+        '¡Bienvenido a este tutorial guiado para enviar mensajes a tus amigos en Facebook! Además de las instrucciones habladas, también puedes ver las instrucciones escritas en la parte izquierda, en la caja de instrucciones, donde encontrarás varios botones para navegar entre los diferentes pasos. Muy bien, entonces empecemos.',
+      audio: '/assets/audio/facebook/enviar_mensajes/paso_1.mp3',
+    },
+    {
+      index: 2,
+      content:
+        'En nuestra pantalla principal del teléfono, presionamos el botón que está siendo destacado de nuestra aplicación de Facebook para continuar con el siguiente paso.',
+      audio: '/assets/audio/facebook/enviar_mensajes/paso_2.mp3',
+    },
+    {
+      index: 3,
+      content:
+        'Una vez abierta la aplicación de Facebook, vamos a presionar el ícono de mensaje señalado en el recuadro rojo. Presiona el ícono de mensaje para continuar.',
+      audio: '/assets/audio/facebook/enviar_mensajes/paso_2.mp3',
+    },
 
+  ];
+  currentIndex = 0;
+
+  onAudioInstruccionesButtonClick(buttonText: string): void {
+    switch (buttonText) {
+      case 'Anterior':
+        this.previousInstruction();
+        break;
+      case 'Siguiente':
+        this.nextInstruction();
+        break;
+      case 'Repetir':
+        this.repeatInstruction();
+        break;
+    }
+  }
+
+  nextInstruction(): void {
+    if (this.currentIndex < this.audioInstruccionesData.length - 1) {
+      this.currentIndex++;
+      this.clearNextInstructionTrigger(); // clear existing timer
+      this.setupNextInstructionTrigger();
+    }
+  }
+
+  previousInstruction(): void {
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+      this.clearNextInstructionTrigger(); // clear existing timer
+      this.setupNextInstructionTrigger();
+    }
+  }
+
+  repeatInstruction(): void {
+    // Handle repeat logic if needed
+  }
+
+  setupNextInstructionTrigger(): void {
+    // Clear any existing triggers
+    this.clearNextInstructionTrigger();
+
+    // Set up trigger based on current index
+    switch (this.currentIndex) {
+      case 0:
+        // Set up a timer for the next step after 30 seconds
+        this.nextInstructionTimer = setTimeout(() => {
+          this.nextInstruction();
+        }, 30000);
+        break;
+      // Add more cases for other steps
+    }
+  }
+
+  ngOnDestroy() {
+    // Unsubscribe to prevent memory leaks
+    if (this.audioSubscription) {
+      this.audioSubscription.unsubscribe();
+    }
+
+    // Clear any existing triggers
+    this.clearNextInstructionTrigger();
+  }
+
+  clearNextInstructionTrigger(): void {
+    // Clear any existing triggers, such as timers or event listeners
+    // This will depend on the specific requirements for clearing triggers
+
+    // Clear the timer if it's set
+    if (this.nextInstructionTimer) {
+      clearTimeout(this.nextInstructionTimer);
+      this.nextInstructionTimer = null; // Reset the timer variable
+    }
+  }
+
+  //Method for advancing to the next step
+  enviarMensajesRegistroClicked(): void {
+    // Call the nextInstruction method or perform other actions specific to index 1
+    this.nextInstruction();
+  }
 }
